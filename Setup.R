93cff354503589df49764cee3c71ae0fc6598a00
Yezzen Khazindar <yezzen.khazindar@colostate.edu>
2023-01-23 08:27:40 -0700

    init repo files

---
+++ Setup.R
@@ -0,0 +1,23 @@
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+# create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'sf',
+              'terra',
+              'tmap',
+              'rmarkdown',
+              'tigris',
+              'elevatr',
+              'rgdal')
+
+packageLoad(packages)
+
